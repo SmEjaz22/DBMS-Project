eea1eb463edcacd2d5d8984623e42719d1ee83a5
SmEjaz22 <110525435+SmEjaz22@users.noreply.github.com>
2023-01-07 00:10:29 +0500

    Add files via upload

---
+++ sample1.py
@@ -0,0 +1,6 @@
+import pyodbc
+import streamlit as st
+
+st.set_page_config(page_title="DBMS", page_icon=":relaxed:", layout="wide")
+st.subheader("What is ARTIFICIAL INTELLIGENCE ?")
+st.write("hey there, starting of dbms project")
