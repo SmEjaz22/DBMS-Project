3a5cf1909a37c4303e16b019a4166def19d18d97
SmEjaz22 <110525435+SmEjaz22@users.noreply.github.com>
2023-01-07 13:31:30 +0500

    Delete sample1.py

--- sample1.py
+++
@@ -1,6 +0,0 @@
-import pyodbc
-import streamlit as st
-
-st.set_page_config(page_title="DBMS", page_icon=":relaxed:", layout="wide")
-st.subheader("What is ARTIFICIAL INTELLIGENCE ?")
-st.write("hey there, starting of dbms project")
